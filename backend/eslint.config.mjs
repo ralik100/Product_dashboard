0a23e190341848710e896fe13d7c47715efc029d
ralik100 <sbagniewski051@gmail.com>
2026-05-17 16:57:19 +0200

    test14

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,7 +3,15 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser, ecmaVersion: "latest",
-      sourceType: "commonjs"} },
+  {
+    files: ["backend/**/*.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
 
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+    },
+  },
 ]);
